7da62e8f5d60a55ae5ae0f87e846e55d3c58d0e2
oliviarutkowski <137834771+oliviarutkowski@users.noreply.github.com>
2025-09-17 16:12:00 -0600

    Initial Commit

---
+++ vetdiagnostics/vetdiagnosticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  vetdiagnosticsApp.swift
+//  vetdiagnostics
+//
+//  Created by Olivia on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct vetdiagnosticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ vetdiagnosticsTests/vetdiagnosticsTests.swift
@@ -0,0 +1,16 @@
+//
+//  vetdiagnosticsTests.swift
+//  vetdiagnosticsTests
+//
+//  Created by Olivia on 9/17/25.
+//
+
+import Testing
+
+struct vetdiagnosticsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
